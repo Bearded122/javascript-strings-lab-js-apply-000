a04ee40092bf1d84efe943ef7743032b69050492
John Sime <johnc.sime@gmail.com>
2018-09-14 22:33:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let's talk about " + `${topic}` + ".";
+var topic = "Let\'s talk about space";
 
 var conversation = `${topic}`;
 
